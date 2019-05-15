8e74af92aa92c7eb7220ef0402b67ac7d1e1d206
Nicolas Ternisien <nicolas.ternisien@gmail.com>
2019-05-15 10:10:01 +0200

    Changes for TNT import

--- 3.3/local.php
+++ 3.3/local.php
@@ -1,15 +1,19 @@
 <?php
- // Paths
- @define('CONST_Postgresql_Version', '11');
- @define('CONST_Postgis_Version', '2.5');
- // Website settings
- @define('CONST_Website_BaseURL', '/');
- @define('CONST_Replication_Url', 'http://download.geofabrik.de/europe/monaco-updates');
- @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
- @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
- @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
- @define('CONST_Pyosmium_Binary', '/usr/local/bin/pyosmium-get-changes');
+// Paths
+@define('CONST_Postgresql_Version', '11');
+@define('CONST_Postgis_Version', '2.5');
+// Website settings
+@define('CONST_Website_BaseURL', '/');
+@define('CONST_Replication_Url', 'http://download.geofabrik.de/europe/monaco-updates');
+@define('CONST_Replication_MaxInterval', '86400'); // Process each update separately, osmosis cannot merge multiple updates
+@define('CONST_Replication_Update_Interval', '86400'); // How often upstream publishes diffs
+@define('CONST_Replication_Recheck_Interval', '900'); // How long to sleep if no update found yet
+@define('CONST_Pyosmium_Binary', '/usr/local/bin/pyosmium-get-changes');
 
- //@define('CONST_Database_DSN', 'pgsql://nominatim:password1234@192.168.1.128:6432/nominatim'); // <driver>://<username>:<password>@<host>:<port>/<database>
+@define('CONST_Osm2pgsql_Flatnode_File', '/data/flatnode.data');
+// @define('CONST_Import_Style', CONST_BasePath.'/settings/import-admin.style');
+@define('CONST_Import_Style', CONST_BasePath . '/settings/import-tracesnt.style');
+
+// @define('CONST_Database_DSN', 'pgsql://nominatim:password1234@192.168.1.128:6432/nominatim'); // <driver>://<username>:<password>@<host>:<port>/<database>
 
 ?>
